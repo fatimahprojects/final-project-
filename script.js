3245a961fbc5710a0ec0ad9e876c3a387e412683
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-26 02:03:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.querySelectorAll('img').forEach(img => {
-    img.setAttribute('tabindex', '0');
-});
+window.addEventListener('load', addTabFocus);
+
+function addTabFocus() {
+    console.log('Tabindex function triggered');
+    document.querySelectorAll('img').forEach(img => {
+        img.setAttribute('tabindex', '0');
+    });
+}
 
 function upDate(previewPic) {
     const imageDiv = document.getElementById('image');
@@ -20,7 +25,6 @@ document.querySelectorAll('img').forEach(img => {
     img.addEventListener('focus', () => upDate(img));
     img.addEventListener('blur', () => unDo());
 });
-
 const images = [
     "crochet_octopus_keychain.jpg",
     "Mini_crochet_octopus_plushies.jpg",
@@ -29,6 +33,7 @@ const images = [
     "Crochet_granny_square_coaster.jpg",
     "Chunky_Puffy_Yarn_Blanket.jpg"
 ];
+
 const alts = [
     "crochet octopus keychain",
     "Mini crochet octopus plushies",
@@ -37,7 +42,9 @@ const alts = [
     "Crochet granny square coaster",
     "Chunky Puffy Yarn Blanket"
 ];
+
 let currentIndex = 0;
+
 document.body.addEventListener('click', () => {
     const imageDiv = document.getElementById('image');
     if (currentIndex < images.length) {
@@ -49,4 +56,4 @@ document.body.addEventListener('click', () => {
         imageDiv.textContent = "Here you can explore my crochet works! Hover over any image below to display it here.";
         currentIndex = 0;
     }
-}); 
+});
