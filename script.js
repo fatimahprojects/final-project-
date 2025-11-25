f4d82b7f72b461a003c43b309593eb19e2ef6e6d
Fatimah Albariqi <fatemahh744@gmail.com>
2025-11-26 01:53:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,33 @@ document.querySelectorAll('img').forEach(img => {
     img.addEventListener('focus', () => upDate(img));
     img.addEventListener('blur', () => unDo());
 });
+
+const images = [
+    "crochet_octopus_keychain.jpg",
+    "Mini_crochet_octopus_plushies.jpg",
+    "Pink_crochet_coaster.jpg",
+    "Crochet_sunflower_coaster.jpg",
+    "Crochet_granny_square_coaster.jpg",
+    "Chunky_Puffy_Yarn_Blanket.jpg"
+];
+const alts = [
+    "crochet octopus keychain",
+    "Mini crochet octopus plushies",
+    "Pink crochet coaster with a heart-shaped center",
+    "Crochet sunflower coaster",
+    "Crochet granny square coaster",
+    "Chunky Puffy Yarn Blanket"
+];
+let currentIndex = 0;
+document.body.addEventListener('click', () => {
+    const imageDiv = document.getElementById('image');
+    if (currentIndex < images.length) {
+        imageDiv.style.backgroundImage = `url(${images[currentIndex]})`;
+        imageDiv.textContent = alts[currentIndex];
+        currentIndex++;
+    } else {
+        imageDiv.style.backgroundImage = "none";
+        imageDiv.textContent = "Here you can explore my crochet works! Hover over any image below to display it here.";
+        currentIndex = 0;
+    }
+}); 
